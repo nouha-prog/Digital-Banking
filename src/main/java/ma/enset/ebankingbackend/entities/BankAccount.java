f896f81163105fd72d88e1fd04e98b3ab30d2d27
nouhaila <nouhaila@exemple.com>
2025-04-28 08:54:25 +0000

    Premiere version du code

--- src/main/java/ma/enset/ebankingbackend/entities/BankAccount.java
+++ src/main/java/ma/enset/ebankingbackend/entities/BankAccount.java
@@ -1,4 +1,20 @@
 package ma.enset.ebankingbackend.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ma.enset.ebankingbackend.enums.AccountStatus;
+
+import java.util.Date;
+import java.util.List;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+
 public class BankAccount {
+        private String id;
+        private double balance;
+        private Date createdAt;
+        private AccountStatus status;
+        private Customer customer;
+        private List<AccountOperation> accountOperations;
 }
