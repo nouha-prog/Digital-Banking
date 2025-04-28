b6adbf3522377eb5dbb8583362915c052fff1f71
nouhaila <nouhaila@exemple.com>
2025-04-28 08:35:43 +0000

    Update Customer.java

--- src/main/java/ma/enset/ebankingbackend/entities/Customer.java
+++ src/main/java/ma/enset/ebankingbackend/entities/Customer.java
@@ -1,4 +1,13 @@
 package ma.enset.ebankingbackend.entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
 }
