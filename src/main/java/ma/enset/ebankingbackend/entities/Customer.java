601845c18a4fbdc7c0c99f3f4050560c301aaddf
nouhaila <nouhaila@exemple.com>
2025-04-28 08:46:08 +0000

    Mise a jour du code

--- src/main/java/ma/enset/ebankingbackend/entities/Customer.java
+++ src/main/java/ma/enset/ebankingbackend/entities/Customer.java
@@ -1,13 +1,21 @@
 package ma.enset.ebankingbackend.entities;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import org.springframework.data.annotation.Id;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
 public class Customer {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
+    @OneToMany(mappedBy = "customer")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private List<BankAccount> bankAccounts;
 }
