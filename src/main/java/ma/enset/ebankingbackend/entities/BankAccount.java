e9b4f0cc9f4e55481d0403953bcd14cc87bb7657
nouhaila <nouhaila@exemple.com>
2025-05-17 15:39:46 +0000

    Modification final

--- src/main/java/ma/enset/ebankingbackend/entities/BankAccount.java
+++ src/main/java/ma/enset/ebankingbackend/entities/BankAccount.java
@@ -1,5 +1,7 @@
 package ma.enset.ebankingbackend.entities;
 
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,14 +9,19 @@ import ma.enset.ebankingbackend.enums.AccountStatus;
 
 import java.util.Date;
 import java.util.List;
-
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "Type",length = 4)
 @Data @AllArgsConstructor @NoArgsConstructor
 
 public class BankAccount {
+        @Id
         private String id;
         private double balance;
         private Date createdAt;
         private AccountStatus status;
+        @ManyToOne
         private Customer customer;
+        @OneToMany(mappedBy = "bankAccount")
         private List<AccountOperation> accountOperations;
 }
