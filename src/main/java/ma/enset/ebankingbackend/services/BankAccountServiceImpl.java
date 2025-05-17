1dff15596159824255c63d02480422b0d036dfc0
nouhaila <nouhaila@exemple.com>
2025-05-17 22:05:27 +0000

    premiere version

---
+++ src/main/java/ma/enset/ebankingbackend/services/BankAccountServiceImpl.java
@@ -0,0 +1,45 @@
+package ma.enset.ebankingbackend.services;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ma.enset.ebankingbackend.entities.BankAccount;
+import ma.enset.ebankingbackend.entities.Customer;
+import ma.enset.ebankingbackend.entities.SavingAccount;
+import ma.enset.ebankingbackend.repositories.AccountOperationRepository;
+import ma.enset.ebankingbackend.repositories.BankAccountRepository;
+import ma.enset.ebankingbackend.repositories.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@AllArgsConstructor
+@Slf4j
+public class BankAccountServiceImpl implements BankAccountService {
+    private CustomerRepository customerRepository;
+    private BankAccountRepository bankAccountRepository;
+    private AccountOperationRepository accountOperationRepository;
+
+
+    @Override
+    public Customer saveCustomer(Customer customer) {
+        log.info("Saving new Customer");
+
+        return null;
+    }
+
+    @Override
+    public BankAccount saveBnakAccount(double initialBalance, String type, Long customer) {
+        BankAccount bankAccount = null; // Ajout de l'initialisation de la variable
+        if(type.equals("current")){
+            bankAccount=new SavingAccount();
+        }
+        // La méthode setId() sans arguments n'est probablement pas correcte
+        // bankAccount.setId();
+        return null;
+    }
+}
