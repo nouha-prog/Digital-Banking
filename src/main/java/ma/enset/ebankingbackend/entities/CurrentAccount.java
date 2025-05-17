a0eaab559852d296438c04f6c56d20bdad170d21
nouhaila <nouhaila@exemple.com>
2025-05-17 17:30:21 +0000

    modification

--- src/main/java/ma/enset/ebankingbackend/entities/CurrentAccount.java
+++ src/main/java/ma/enset/ebankingbackend/entities/CurrentAccount.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@DiscriminatorValue("CA")
+//@DiscriminatorValue("CA")
 @Data @NoArgsConstructor @AllArgsConstructor
 public class CurrentAccount extends BankAccount {
     private double overDraft;
