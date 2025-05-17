efe91d68099b35634c5c0ebb6b7894b0bd9df236
nouhaila <nouhaila@exemple.com>
2025-05-17 23:08:23 +0000

    premiere version

---
+++ src/main/java/ma/enset/ebankingbackend/web/CustomerRestController.java
@@ -0,0 +1,26 @@
+package ma.enset.ebankingbackend.web;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ma.enset.ebankingbackend.entities.Customer;
+import ma.enset.ebankingbackend.services.BankAccountService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+@CrossOrigin("*")
+public class CustomerRestController {
+    private BankAccountService bankAccountService;
+    @GetMapping("/customers")
+
+    public List<Customer> getCustomers(){
+        log.info("Getting all customers");
+        return bankAccountService.listCustomers();
+    }
+}
+
