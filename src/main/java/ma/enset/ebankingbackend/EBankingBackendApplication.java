5ec34abd25c8e37458e4af8df2bc9abf45d99f78
nouhaila <nouhaila@exemple.com>
2025-05-17 16:12:43 +0000

    Mise a jour du code

--- src/main/java/ma/enset/ebankingbackend/EBankingBackendApplication.java
+++ src/main/java/ma/enset/ebankingbackend/EBankingBackendApplication.java
@@ -1,7 +1,14 @@
 package ma.enset.ebankingbackend;
 
+import ma.enset.ebankingbackend.entities.Customer;
+import ma.enset.ebankingbackend.repositories.AccountOperationRepository;
+import ma.enset.ebankingbackend.repositories.BankAccountRepository;
+import ma.enset.ebankingbackend.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class EBankingBackendApplication {
@@ -10,4 +17,17 @@ public class EBankingBackendApplication {
         SpringApplication.run(EBankingBackendApplication.class, args);
     }
 
+    //@Bean
+    CommandLineRunner start(CustomerRepository customerRepository,
+                            BankAccountRepository bankAccountRepository,
+                            AccountOperationRepository accountOperationRepository) {
+        return args -> {
+            Stream.of("Hassan", "Yassine", "Aicha").forEach(name -> {
+                Customer customer = new Customer();
+                customer.setName(name);
+                customer.setEmail(name + "@gmail.com");
+                customerRepository.save(customer);
+            });
+        };
+    }
 }
