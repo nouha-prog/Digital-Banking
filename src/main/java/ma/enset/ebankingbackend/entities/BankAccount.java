8bf2d4b41c6ccef272aaaa67e5953c2ca312f5a9
nouhaila <nouhaila@exemple.com>
2025-05-17 20:19:09 +0000

    Amelioration

--- src/main/java/ma/enset/ebankingbackend/entities/BankAccount.java
+++ src/main/java/ma/enset/ebankingbackend/entities/BankAccount.java
@@ -7,21 +7,23 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import ma.enset.ebankingbackend.enums.AccountStatus;
 
+
 import java.util.Date;
 import java.util.List;
-@Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-//@DiscriminatorColumn(name = "Type",length = 4)
-@Data @AllArgsConstructor @NoArgsConstructor
 
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "TYPE",length = 4)
+@Data @NoArgsConstructor @AllArgsConstructor
 public abstract class BankAccount {
         @Id
         private String id;
         private double balance;
         private Date createdAt;
+        @Enumerated(EnumType.STRING)
         private AccountStatus status;
         @ManyToOne
         private Customer customer;
-        @OneToMany(mappedBy = "bankAccount")
+        @OneToMany(mappedBy = "bankAccount",fetch = FetchType.LAZY)
         private List<AccountOperation> accountOperations;
 }
