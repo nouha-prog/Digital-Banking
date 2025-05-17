53a1b4cb25f70a17af0d9a5b21632f11676161a9
nouhaila <nouhaila@exemple.com>
2025-05-17 16:03:42 +0000

    Ajout de l´interface AccountOperationRepository

--- src/main/java/ma/enset/ebankingbackend/repositories/AccountOperationRepository.java
+++ src/main/java/ma/enset/ebankingbackend/repositories/AccountOperationRepository.java
@@ -1,4 +1,5 @@
 package ma.enset.ebankingbackend.repositories;
+import ma.enset.ebankingbackend.entities.AccountOperation;
 import ma.enset.ebankingbackend.entities.BankAccount;
 import ma.enset.ebankingbackend.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
+public interface AccountOperationRepository extends JpaRepository<AccountOperation,Long> {
     @Query("select c from Customer c where c.name like :kw")
     List<Customer> searchCustomer(@Param("kw") String keyword);
 }
