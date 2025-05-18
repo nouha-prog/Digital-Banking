a7c6b658721a40c0c681e6eb81306e1c4a506100
nouhaila <nouhaila@exemple.com>
2025-05-18 13:36:35 +0000

    Ajout des DTO

--- src/main/java/ma/enset/ebankingbackend/dto/CustomerDTO.java
+++ src/main/java/ma/enset/ebankingbackend/dto/CustomerDTO.java
@@ -1,4 +1,10 @@
 package ma.enset.ebankingbackend.dto;
 
-public class CustomerDto {
+import lombok.Data;
+
+@Data
+public class CustomerDTO {
+    private Long id;
+    private String name;
+    private String email;
 }
