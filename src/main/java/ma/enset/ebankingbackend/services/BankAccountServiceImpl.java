46ff68ba8ffd04d631c355ef146631e8248fe7d4
nouhaila <nouhaila@exemple.com>
2025-05-18 12:47:15 +0000

    Ajout des Exceptions

--- src/main/java/ma/enset/ebankingbackend/services/BankAccountServiceImpl.java
+++ src/main/java/ma/enset/ebankingbackend/services/BankAccountServiceImpl.java
@@ -2,9 +2,12 @@ package ma.enset.ebankingbackend.services;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ma.enset.ebankingbackend.entities.AccountOperation;
 import ma.enset.ebankingbackend.entities.BankAccount;
 import ma.enset.ebankingbackend.entities.Customer;
 import ma.enset.ebankingbackend.entities.SavingAccount;
+import ma.enset.ebankingbackend.enums.OperationType;
+import ma.enset.ebankingbackend.exceptions.BankAccountNotFoundException;
 import ma.enset.ebankingbackend.repositories.AccountOperationRepository;
 import ma.enset.ebankingbackend.repositories.BankAccountRepository;
 import ma.enset.ebankingbackend.repositories.CustomerRepository;
@@ -12,6 +15,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,12 +39,28 @@ public class BankAccountServiceImpl implements BankAccountService {
 
     @Override
     public BankAccount saveBnakAccount(double initialBalance, String type, Long customer) {
-        BankAccount bankAccount = null; // Ajout de l'initialisation de la variable
+        BankAccount bankAccount = null;
         if(type.equals("current")){
             bankAccount=new SavingAccount();
         }
-        // La méthode setId() sans arguments n'est probablement pas correcte
-        // bankAccount.setId();
+
         return null;
+    }
+
+    @Override
+    public void debit(String accountId, double amount, String description) throws BankAccountNotFoundException, BalanceNotSufficientException {
+        BankAccount bankAccount=bankAccountRepository.findById(accountId)
+                .orElseThrow(()->new BankAccountNotFoundException("BankAccount not found"));
+        if(bankAccount.getBalance()<amount)
+            throw new BalanceNotSufficientException("Balance not sufficient");
+        AccountOperation accountOperation=new AccountOperation();
+        accountOperation.setType(OperationType.DEBIT);
+        accountOperation.setAmount(amount);
+        accountOperation.setDescription(description);
+        accountOperation.setOperationDate(new Date());
+        accountOperation.setBankAccount(bankAccount);
+        accountOperationRepository.save(accountOperation);
+        bankAccount.setBalance(bankAccount.getBalance()-amount);
+        bankAccountRepository.save(bankAccount);
     }
 }
