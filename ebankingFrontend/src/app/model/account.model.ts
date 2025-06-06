67698eb005a49f5df5deadcd46c83d17987ef7d9
nouha-prog <nouhailaelhajji488@gmail.com>
2025-06-06 19:01:06 +0100

    Update account.model.ts

--- ebankingFrontend/src/app/model/account.model.ts
+++ ebankingFrontend/src/app/model/account.model.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { Customer } from "./customer.model";
 
 export interface Account {
@@ -19,29 +19,29 @@ export interface AccountDetails {
   currentPage: number;
   totalPages: number;
   pageSize: number;
-=======
+
 export interface AccountDetails {
   accountId:            string;
   balance:              number;
   currentPage:          number;
   totalPages:           number;
   pageSize:             number;
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
   accountOperationDTOS: AccountOperation[];
 }
 
 export interface AccountOperation {
-<<<<<<< HEAD
+
   id: number;
   operationDate: Date;
   amount: number;
   type: string;
   description: string;
-=======
+
   id:            number;
   operationDate: Date;
   amount:        number;
   type:          string;
   description:   string;
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
 }
