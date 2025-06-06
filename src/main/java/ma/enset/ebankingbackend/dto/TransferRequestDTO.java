ea84e44346cd515165c0997651a680ab9a2890a5
nouha-prog <nouhailaelhajji488@gmail.com>
2025-06-06 18:24:27 +0100

    Update TransferRequestDTO.java

--- src/main/java/ma/enset/ebankingbackend/dto/TransferRequestDTO.java
+++ src/main/java/ma/enset/ebankingbackend/dto/TransferRequestDTO.java
@@ -1,9 +1,6 @@
 package ma.enset.ebankingbackend.dto;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
 import lombok.Data;
 
 @Data
@@ -12,8 +9,8 @@ public class TransferRequestDTO {
     private String accountDestination;
     private double amount;
     private String description;
-<<<<<<< HEAD
+
 }
-=======
+
 }
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
