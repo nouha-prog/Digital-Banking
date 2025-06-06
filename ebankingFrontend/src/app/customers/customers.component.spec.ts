93aa87e4b427c39f062f3a0a35047bd4aad9d463
nouha-prog <nouhailaelhajji488@gmail.com>
2025-06-06 18:58:38 +0100

    Update customers.component.spec.ts

--- ebankingFrontend/src/app/customers/customers.component.spec.ts
+++ ebankingFrontend/src/app/customers/customers.component.spec.ts
@@ -6,12 +6,12 @@ describe('CustomersComponent', () => {
   let component: CustomersComponent;
   let fixture: ComponentFixture<CustomersComponent>;
 
-<<<<<<< HEAD
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [CustomersComponent]
     });
-=======
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ CustomersComponent ]
@@ -20,7 +20,7 @@ describe('CustomersComponent', () => {
   });
 
   beforeEach(() => {
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
     fixture = TestBed.createComponent(CustomersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
