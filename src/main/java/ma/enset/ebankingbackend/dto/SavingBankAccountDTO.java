cde6478f7b668f2577068e686ff684f73f18b04e
nouha-prog <nouhailaelhajji488@gmail.com>
2025-06-06 18:23:54 +0100

    Update SavingBankAccountDTO.java

--- src/main/java/ma/enset/ebankingbackend/dto/SavingBankAccountDTO.java
+++ src/main/java/ma/enset/ebankingbackend/dto/SavingBankAccountDTO.java
@@ -2,10 +2,9 @@ package ma.enset.ebankingbackend.dto;
 
 import lombok.Data;
 import ma.enset.ebankingbackend.enums.AccountStatus;
-<<<<<<< HEAD
-=======
 
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
+
 import java.util.Date;
 
 @Data
@@ -14,10 +13,9 @@ public class SavingBankAccountDTO extends BankAccountDTO {
     private double balance;
     private Date createdAt;
     private AccountStatus status;
-<<<<<<< HEAD
+
     private String currency;
-=======
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
     private CustomerDTO customerDTO;
     private double interestRate;
 }
