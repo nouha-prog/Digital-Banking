e6bb1343ac03bd405b231dd5897072793ad646ad
nouha-prog <nouhailaelhajji488@gmail.com>
2025-06-06 18:28:17 +0100

    Update OperationType.java

--- src/main/java/ma/enset/ebankingbackend/enums/OperationType.java
+++ src/main/java/ma/enset/ebankingbackend/enums/OperationType.java
@@ -1,10 +1,10 @@
 package ma.enset.ebankingbackend.enums;
 
 public enum OperationType {
-<<<<<<< HEAD
+
     DEBIT, CREDIT, TRANSFER
 }
-=======
+
     DEBIT, CREDIT
 }
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
