af36a291c9067cbe241cac7a731a2d5ec591bf3f
nouha-prog <nouhailaelhajji488@gmail.com>
2025-06-06 18:39:30 +0100

    Update BankAccountRepository.java

--- src/main/java/ma/enset/ebankingbackend/repositories/BankAccountRepository.java
+++ src/main/java/ma/enset/ebankingbackend/repositories/BankAccountRepository.java
@@ -1,5 +1,5 @@
 package ma.enset.ebankingbackend.repositories;
-<<<<<<< HEAD
+
 
 import ma.enset.ebankingbackend.entities.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
     List<BankAccount> findByCustomerId(Long customerId);
-=======
+
 import ma.enset.ebankingbackend.entities.BankAccount;
 import ma.enset.ebankingbackend.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -19,5 +19,5 @@ import java.util.List;
 
 
 public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
 }
