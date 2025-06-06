001b277b97ca1e115c41a7c147b92eb1e9beda39
nouha-prog <nouhailaelhajji488@gmail.com>
2025-06-06 18:27:08 +0100

    Update Customer.java

--- src/main/java/ma/enset/ebankingbackend/entities/Customer.java
+++ src/main/java/ma/enset/ebankingbackend/entities/Customer.java
@@ -18,8 +18,8 @@ public class Customer {
     @OneToMany(mappedBy = "customer")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private List<BankAccount> bankAccounts;
-<<<<<<< HEAD
+
 }
-=======
+
 }
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
