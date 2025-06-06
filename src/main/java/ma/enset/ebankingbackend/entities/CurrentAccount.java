96cdcf24007754231c838721d1c4bfa81c70dee1
nouha-prog <nouhailaelhajji488@gmail.com>
2025-06-06 18:26:33 +0100

    Update CurrentAccount.java

--- src/main/java/ma/enset/ebankingbackend/entities/CurrentAccount.java
+++ src/main/java/ma/enset/ebankingbackend/entities/CurrentAccount.java
@@ -9,13 +9,13 @@ import lombok.NoArgsConstructor;
 @Entity
 @DiscriminatorValue("CA")
 @Data @NoArgsConstructor @AllArgsConstructor
-<<<<<<< HEAD
+
 public class CurrentAccount extends BankAccount{
     private double overDraft;
 }
 
-=======
+
 public class CurrentAccount extends BankAccount {
     private double overDraft;
 }
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
