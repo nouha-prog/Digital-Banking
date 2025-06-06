1882c91fca04a84bae846975ce6792b07ae69d26
nouha-prog <nouhailaelhajji488@gmail.com>
2025-06-06 18:37:29 +0100

    Update CustomerNotFoundException.java

--- src/main/java/ma/enset/ebankingbackend/exceptions/CustomerNotFoundException.java
+++ src/main/java/ma/enset/ebankingbackend/exceptions/CustomerNotFoundException.java
@@ -4,8 +4,8 @@ public class CustomerNotFoundException extends Exception {
     public CustomerNotFoundException(String message) {
         super(message);
     }
-<<<<<<< HEAD
+
 }
-=======
+
 }
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
