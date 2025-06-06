0b4ab805c0aa08b3c91d6f3ecbeee20f69098468
nouha-prog <nouhailaelhajji488@gmail.com>
2025-06-06 18:36:10 +0100

    Update BalanceNotSufficientException.java

--- src/main/java/ma/enset/ebankingbackend/exceptions/BalanceNotSufficientException.java
+++ src/main/java/ma/enset/ebankingbackend/exceptions/BalanceNotSufficientException.java
@@ -4,8 +4,8 @@ public class BalanceNotSufficientException extends Exception {
     public BalanceNotSufficientException(String message) {
         super(message);
     }
-<<<<<<< HEAD
+
 }
-=======
+
 }
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
