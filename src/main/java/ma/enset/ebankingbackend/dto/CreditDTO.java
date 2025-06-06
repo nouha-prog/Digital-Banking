ab052b11a1f675ed8c157d7128ec8db36f4103f5
nouha-prog <nouhailaelhajji488@gmail.com>
2025-06-06 18:04:19 +0100

    Update CreditDTO.java

--- src/main/java/ma/enset/ebankingbackend/dto/CreditDTO.java
+++ src/main/java/ma/enset/ebankingbackend/dto/CreditDTO.java
@@ -7,8 +7,8 @@ public class CreditDTO {
     private String accountId;
     private double amount;
     private String description;
-<<<<<<< HEAD
+
 }
-=======
+
 }
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
