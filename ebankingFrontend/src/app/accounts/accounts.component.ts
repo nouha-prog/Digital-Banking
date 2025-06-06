b9fd8778a775cbd3bba39ed90e181bee69882c22
nouha-prog <nouhailaelhajji488@gmail.com>
2025-06-06 18:55:37 +0100

    Update accounts.component.ts

--- ebankingFrontend/src/app/accounts/accounts.component.ts
+++ ebankingFrontend/src/app/accounts/accounts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
+
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { catchError, Observable, throwError } from 'rxjs';
 import { AccountDetails } from '../model/account.model';
@@ -7,43 +7,43 @@ import { AccountService } from '../services/accounts.service';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../services/auth.service';
 
-=======
+
 import { FormBuilder, FormGroup } from "@angular/forms";
 import { AccountsService } from "../services/accounts.service";
 import { catchError, Observable, throwError } from "rxjs";
 import { AccountDetails } from "../model/account.model";
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
 
 @Component({
   selector: 'app-accounts',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './accounts.component.html',
-<<<<<<< HEAD
+
   styleUrl: './accounts.component.css'
-=======
+
   styleUrls: ['./accounts.component.css']
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
 })
 export class AccountsComponent implements OnInit {
   accountFormGroup!: FormGroup;
   currentPage: number = 0;
   pageSize: number = 5;
-<<<<<<< HEAD
+
   accountObservable!: Observable<AccountDetails>
   operationFromGroup!: FormGroup;
   errorMessage!: string ;
 
   constructor(private fb : FormBuilder, private accountService : AccountService, public authService: AuthService) { }
-=======
+
   accountObservable!: Observable<AccountDetails>;
   operationFromGroup!: FormGroup;
   errorMessage!: string;
 
   constructor(private fb: FormBuilder, private accountService: AccountsService) { }
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
 
   ngOnInit(): void {
     this.accountFormGroup = this.fb.group({
@@ -54,7 +54,7 @@ export class AccountsComponent implements OnInit {
       amount: this.fb.control(0),
       description: this.fb.control(null),
       accountDestination: this.fb.control(null)
-<<<<<<< HEAD
+
     })
   }
 
@@ -64,7 +64,7 @@ export class AccountsComponent implements OnInit {
       catchError(err => {
         this.errorMessage = err.message;
         return throwError(() => err);
-=======
+
     });
   }
 
@@ -74,7 +74,7 @@ export class AccountsComponent implements OnInit {
       catchError(err => {
         this.errorMessage = err.message;
         return throwError(err);
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
       })
     );
   }
@@ -90,21 +90,21 @@ export class AccountsComponent implements OnInit {
     let amount: number = this.operationFromGroup.value.amount;
     let description: string = this.operationFromGroup.value.description;
     let accountDestination: string = this.operationFromGroup.value.accountDestination;
-<<<<<<< HEAD
+
 
     if(operationType=='DEBIT'){
       this.accountService.debit(accountId, amount, description).subscribe({
         next : (data) =>{
-=======
+
     if (operationType == 'DEBIT') {
       this.accountService.debit(accountId, amount, description).subscribe({
         next: (data) => {
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
           alert("Success Credit");
           this.operationFromGroup.reset();
           this.handleSearchAccount();
         },
-<<<<<<< HEAD
+
         error : (err) => {
           console.log(err);
         }
@@ -112,7 +112,7 @@ export class AccountsComponent implements OnInit {
     } else if(operationType=='CREDIT'){
       this.accountService.credit(accountId, amount, description).subscribe({
         next : (data) => {
-=======
+
         error: (err) => {
           console.log(err);
         }
@@ -120,38 +120,38 @@ export class AccountsComponent implements OnInit {
     } else if (operationType == 'CREDIT') {
       this.accountService.credit(accountId, amount, description).subscribe({
         next: (data) => {
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
           alert("Success Debit");
           this.operationFromGroup.reset();
           this.handleSearchAccount();
         },
-<<<<<<< HEAD
+
         error : (err) => {
-=======
+
         error: (err) => {
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
           console.log(err);
         }
       });
     }
-<<<<<<< HEAD
+
     else if(operationType=='TRANSFER'){
       this.accountService.transfer(accountId,accountDestination, amount,description).subscribe({
         next : (data) => {
-=======
+
     else if (operationType == 'TRANSFER') {
       this.accountService.transfer(accountId, accountDestination, amount, description).subscribe({
         next: (data) => {
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
           alert("Success Transfer");
           this.operationFromGroup.reset();
           this.handleSearchAccount();
         },
-<<<<<<< HEAD
+
         error : (err) => {
-=======
+
         error: (err) => {
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
           console.log(err);
         }
       });
