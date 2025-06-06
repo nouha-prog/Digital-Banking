127aa965409fce66fdc9cfba6701254c849034a5
nouha-prog <nouhailaelhajji488@gmail.com>
2025-06-06 18:59:44 +0100

    Update customers.component.ts

--- ebankingFrontend/src/app/customers/customers.component.ts
+++ ebankingFrontend/src/app/customers/customers.component.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { CustomerService } from '../services/customer.service';
@@ -8,7 +8,7 @@ import { Customer } from '../model/customer.model';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-=======
+
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { CustomerService } from "../services/customer.service";
@@ -18,23 +18,23 @@ import { FormBuilder, FormGroup } from "@angular/forms";
 import { Router } from "@angular/router";
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
 
 @Component({
   selector: 'app-customers',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './customers.component.html',
-<<<<<<< HEAD
+
   styleUrl: './customers.component.css'
-=======
+
   styleUrls: ['./customers.component.css']
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
 })
 export class CustomersComponent implements OnInit {
   customers!: Observable<Array<Customer>>;
   errorMessage!: string;
-<<<<<<< HEAD
+
   searchFormGroup : FormGroup | undefined;
 
   constructor(private customerService: CustomerService, private fb: FormBuilder, private router: Router, public authService: AuthService) {}
@@ -46,7 +46,7 @@ export class CustomersComponent implements OnInit {
     this.handleSearchCustomers();
   }
 
-=======
+
   searchFormGroup: FormGroup | undefined;
   constructor(private customerService: CustomerService, private fb: FormBuilder, private router: Router) { }
 
@@ -56,25 +56,25 @@ export class CustomersComponent implements OnInit {
     });
     this.handleSearchCustomers();
   }
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
   handleSearchCustomers() {
     let kw = this.searchFormGroup?.value.keyword;
     this.customers = this.customerService.searchCustomers(kw).pipe(
       catchError(err => {
-<<<<<<< HEAD
+
         this.errorMessage=err.message;
         return throwError(() => err);
-=======
+
         this.errorMessage = err.message;
         return throwError(err);
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
       })
     );
   }
 
   handleDeleteCustomer(c: Customer) {
     let conf = confirm("Are you sure?");
-<<<<<<< HEAD
+
     if(!conf) return;
     this.customerService.deleteCustomer(c.id).subscribe({
       next : (resp) => {
@@ -82,7 +82,7 @@ export class CustomersComponent implements OnInit {
           map(data =>{
             let index = data.indexOf(c);
             data.slice(index, 1)
-=======
+
     if (!conf) return;
     this.customerService.deleteCustomer(c.id).subscribe({
       next: (resp) => {
@@ -90,12 +90,12 @@ export class CustomersComponent implements OnInit {
           map(data => {
             let index = data.indexOf(c);
             data.slice(index, 1);
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
             return data;
           })
         );
       },
-<<<<<<< HEAD
+
       error : err => {
         console.log(err);
       }
@@ -104,7 +104,7 @@ export class CustomersComponent implements OnInit {
 
   handleCustomerAccounts(customer: Customer) {
     this.router.navigateByUrl(`/admin/customer-accounts/${customer.id}`, {state: customer});
-=======
+
       error: err => {
         console.log(err);
       }
@@ -113,6 +113,6 @@ export class CustomersComponent implements OnInit {
 
   handleCustomerAccounts(customer: Customer) {
     this.router.navigateByUrl("/customer-accounts/" + customer.id, { state: customer });
->>>>>>> 5a4163c096d1ffc6b51c0bdc56f9367f35f69df4
+
   }
 }
